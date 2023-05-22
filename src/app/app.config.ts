5d01806fd0184d0fa3593da69e71e19be273458e
Baruch Giverts <bor770@gmail.com>
2023-05-22 19:11:26 +0300

    advice store

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,10 +4,12 @@ import { provideEffects } from '@ngrx/effects';
 import { provideStore } from '@ngrx/store';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
 
+import { AdviceEffects } from './advice/store/advice.effects';
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideAnimations(),
-    provideEffects(),
+    provideEffects(AdviceEffects),
     provideStore(),
     provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
   ],
